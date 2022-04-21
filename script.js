95ba56edfae6b91f98cd2110291b428774a19309
Catalin Cirlea <catalin.cirlea@ppb.com>
2022-04-21 18:08:04 +0300

    Update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const API_Key = "0067700871cbf561cafaad8074cdc569";
+const API_GEO ='AIzaSyCUKampTzil86ESWG2z0XOI401phK2C-3I'
 const searchName = document.getElementById('city');
 const submitCity = document.getElementById('form');
 const pName = document.getElementById("city-name");
@@ -9,23 +10,35 @@ const tempMin = document.getElementById('temp-min');
 const tempMax = document.getElementById('temp-max');
 const mainIcon = document.getElementById('main-icon');
 
+
 submitCity.addEventListener("submit",()=>{
   if(searchName.value!==""){
       getWeather(searchName.value);
+      // getCoord(searchName.value);
       searchName.value = '';
   }
 });
 
 async function getWeather(city){
   try {
+    // Remove the content when the data is loading
       pName.textContent = "Loading..."
       pCondition.textContent = '';
       pC.textContent = '';
+      tempMin.textContent='';
+      tempMax.textContent='';
+      mainIcon.style.visibility = 'hidden';
+
       const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_Key+"&units=metric",{mode:'cors'});
       const cityInfo = await response.json();
+
+      mainIcon.style.visibility = 'visible';
+      const icon = cityInfo.weather[0].icon;
+      mainIcon.src= "img/"+ icon+".svg"
+      mainIcon.classList.add('fetched');
       pName.textContent = cityInfo.name+" | "+cityInfo.sys.country;
       pCondition.textContent = cityInfo.weather[0].description;
-      pC.textContent = cityInfo.main.temp + "°"; 
+      pC.textContent = Math.round(cityInfo.main.temp) + "°"; 
       tempMin.textContent = Math.round(cityInfo.main.temp_min) + "°";
       tempMax.textContent = Math.round(cityInfo.main.temp_max) + "°";
       console.log(cityInfo);
@@ -34,3 +47,5 @@ async function getWeather(city){
       pCondition.textContent = "Try again"
   }
 };
+
+getWeather('turda');
